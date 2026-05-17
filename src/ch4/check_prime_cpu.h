d15482b1a706b4bdcbfeb4755cb87e26f9318141
Diivanand Ramalingam <diivanand@gmail.com>
2026-05-16 22:04:39 -0700

    switch to pragma once

--- src/ch4/check_prime_cpu.h
+++ src/ch4/check_prime_cpu.h
@@ -1,10 +1,5 @@
-//
-// Created by diiv on 5/16/26.
-//
+#pragma once
 
-#ifndef CUDA_PRACTICE_CHECK_PRIME_CPU_H
-#define CUDA_PRACTICE_CHECK_PRIME_CPU_H
 // Returns true if num is prime, false otherwise.
 // Intended as a CPU reference implementation for validating GPU results.
 bool check_prime_cpu(long long num);
-#endif //CUDA_PRACTICE_CHECK_PRIME_CPU_H
--- src/ch4/check_prime_gpu.cuh
+++ src/ch4/check_prime_gpu.cuh
@@ -1,9 +1,4 @@
-//
-// Created by diiv on 5/16/26.
-//
+#pragma once
 
-#ifndef CUDA_PRACTICE_CHECK_PRIME_GPU_CUH
-#define CUDA_PRACTICE_CHECK_PRIME_GPU_CUH
 // Launches a CUDA kernel that tests each integer in [start, end] for primality.
 void check_prime_gpu_kernel(long long start, long long end);
-#endif //CUDA_PRACTICE_CHECK_PRIME_GPU_CUH
